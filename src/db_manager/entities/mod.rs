08881d0eed30ce82f8801669638f60366f1d1a3d
Pascal Flores <pascalthomas.flores@proton.me>
2024-12-19 23:43:21 +0100

    added derive for entites automatizing new function creation

--- src/db_manager/entities/mod.rs
+++ src/db_manager/entities/mod.rs
@@ -1,52 +1,62 @@
 use std::path::Path;
 
 use chrono::NaiveDate;
+use derive_new::new;
 
+#[derive(new)]
 pub struct Document<'a> {
     id : i32,
     category : &'a Category<'a>,
     author : Author,
     series : Series,
     date : NaiveDate,
-    path : Path
+    path : &'a Path
 } 
 
+#[derive(new)]
 pub struct Category<'a> {
     id : i32,
     name : &'a String,
-    path : Path
+    path : &'a Path
 }
 
+#[derive(new)]
 pub struct Genre {
     id : i32,
     name : String
 }
 
+#[derive(new)]
 pub struct GenreDocument<'a> {
     document : &'a Document<'a>,
     genre : &'a Genre
 }
 
+#[derive(new, PartialEq, Debug)]
 pub struct Author {
     id : i32,
     name : String
 }
 
+#[derive(new)]
 pub struct Series {
     id : i32,
     name : String
 }
 
+#[derive(new)]
 pub struct Tag {
     id : i32,
     name : String
 }
 
+#[derive(new)]
 pub struct AuthorSeries {
     author : Author,
     series : Series
 }
 
+#[derive(new)]
 pub struct DocumentTag<'a> {
     document : &'a Document<'a>,
     tag : Tag
